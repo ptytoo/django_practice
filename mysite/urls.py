012f0861ed25f673c61831f625a421d425c7f37a
ptytoo <ptytoo@naver.com>
2020-07-13 15:48:07 +0900

    fix login import error

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, include
+# from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'', include('blog.urls')),
 ]
